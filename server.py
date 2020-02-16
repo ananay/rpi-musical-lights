20e09f32176556e9acefbcac04cabec9e38a45bb
ananay <i@ananayarora.com>
2020-02-16 05:19:24 -0700

    Added other endpoints

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import subprocess
 
 app = Flask(__name__)
-mode = None
+mode = 'off'
 
 @app.route("/")
 def hello():
@@ -14,11 +14,27 @@ def spectrum():
     mode = 'spectrum'
     return ('spectrum')
 
+@app.route("/energy")
+def energy():
+    subprocess.Popen(['sudo','python', 'python/visualization.py', 'energy'], stdout=subprocess.PIPE)
+    mode = 'energy'
+    return ('energy')
+
+@app.route("/scroll")
+def scroll():
+    subprocess.Popen(['sudo','python', 'python/visualization.py', 'scroll'], stdout=subprocess.PIPE)
+    mode = 'scroll'
+    return ('scroll')
+
+
+@app.route("/status"):
+    return (mode)
+
 @app.route("/kill")
 def kill():
     global pp
     subprocess.Popen(['killall', 'python'])
-    mode = None
+    mode = 'off'
     return('off')
 
 if __name__ == '__main__':
--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,36 @@ $(document).ready(() => {
         $(e.target).addClass('active');
     });
 
+    $.get('/status', (r) => {
+        $(".box").removeClass('active');
+        $('#' + r).addClass('active');
+    })
+
     $("#spectrum").click(() => {
         $.get('/spectrum', (r) => {
-            console.log(r);
+            $(".box").removeClass('active');
+            $("#spectrum").addClass('active');
+        });
+    });
+
+    $("#scroll").click(() => {
+        $.get('/scroll', (r) => {
+            $(".box").removeClass('active');
+            $("#scroll").addClass('active');
+        });
+    });
+
+    $("#energy").click(() => {
+        $.get('/energy', (r) => {
+            $(".box").removeClass('active');
+            $("#energy").addClass('active');
+        });
+    });
+
+    $("#off").click(() => {
+        $.get('/kill', (r) => {
+            $(".box").removeClass('active');
+            $("#off").addClass('active');
         });
     });
 
